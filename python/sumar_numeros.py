742d8ece23e0b54d9bfff51e21327feae35a01d3
Jose Ricardo Mendoza Villar <richi_1325@hotmail.com>
2025-08-20 19:07:10 -0600

    Python: Programa que suma dos números

---
+++ python/sumar_numeros.py
@@ -0,0 +1,5 @@
+if __name__ == "__main__":
+    number1 = int(input("Ingresa el primer numero:"))
+    number2 = int(input("Ingresa el segundo numero:"))
+    suma = number1 + number2
+    print("La suma es:", number1 + number2)
