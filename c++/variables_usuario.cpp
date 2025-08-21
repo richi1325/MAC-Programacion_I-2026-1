4e09e2942aaef22be1eb701766f5617e0bdf0a0f
Jose Ricardo Mendoza Villar <richi_1325@hotmail.com>
2025-08-20 19:06:18 -0600

    C++: Solicitar un valor al usuario

---
+++ c++/variables_usuario.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+
+int main(){
+    int user_number;
+    std::cout << "Ingresa un número entero:";
+    std::cin >> user_number;
+    std::cout << "Tu número es:" << user_number;
+}
