a305dc4dd01939d6619785aed96cf5a6b80b3a1a
Jose Ricardo Mendoza Villar <richi_1325@hotmail.com>
2025-09-17 20:16:27 -0600

    C: Característica adicional de scanf

---
+++ c/6.scanf_prototipado.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+    float a, b, c;
+    printf("Ingresa los coeficientes a_b_c:");
+    scanf("%f_%f_%f",&a,&b,&c);
+    printf("%f %f %f",a,b,c);
+    return 0;
+}
